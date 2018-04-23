997691933b8d6524ed3bd55ff6deb7c1f2b5609e
Francois Chaumont <funcois@outlook.com>
2018-04-22 22:38:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Open Route Service - interface with some functionalities of the API
+PHP library to geocode addresses and compute distances using the Open Route Service API.
+
+## Getting started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Requirements
+PHP 7.0+
+Open Route Service API key
+
+Before being able to use the API, consequently this library as well, you need to create an account with Open Route Service and request an API key. Please visit their official website in order to do so: https://openrouteservice.org
+
+### Installation
+Just include the library and you are ready to go.
+
+## Testing
+A tests file is provided under the tests folder to give an overall idea how to use this library.
+Not intented to be run.
+
+## Built with
+* Visual Studio Code
+
+## Authors
+* **Francois Chaumont** - *Initial work* - [FrancoisChaumont](https://github.com/FrancoisChaumont)
+
+See also the list of [contributors](https://github.com/FrancoisChaumont/db/graphs/contributors) who particpated in this project.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Notes
+TODO: 
+- Handle Exceptions properly
+- Refactor to make it more general and encapsulate more 
---
+++ tests/tests.php
@@ -0,0 +1,50 @@
+<?php
+
+require __DIR__."/../src/OpenRouteService.php";
+
+$sTxtForLogs = '';
+$response = '';
+
+// your API key
+$sApiKey = '123';
+
+// source address
+$sourceStreet = '4046 N Leavitt St';
+$sourceLocality = 'Chicago';
+$sourceRegion = 'IL';
+$sourceZipCode = '60618';
+$sourceCountry = 'USA';
+
+// destination address
+$destStreet = '5454 N Wolcott Ave';
+$destLocality = 'Chicago';
+$destRegion = 'IL';
+$destZipCode = '60640';
+$destCountry = 'USA';
+
+$cOrs = new OpenRouteService($sApiKey);
+
+$source = 0;
+// create a new object address with the source location
+$cAddrSource = new Address($sourceStreet, $sourceLocality, $sourceRegion, $sourceZipCode, $sourceCountry);
+// geocode the source address
+$cCoordSource = $cOrs->geocode($cAddrSource, $sTxtForLogs);
+if ($cCoordSource == null) { die("Error geocoding home location".PHP_EOL.$sTxtForLogs); }
+
+$destination = 1;
+// create a new object address with the destination location
+$cAddrDest = new Address($destStreet, $destLocality, $destRegion, $destZipCode, $destCountry);
+// geocode the destination address
+$cCoordDest = $cOrs->geocode($cAddrDest, $sTxtForLogs);
+if ($cCoordDest == null) { die("Error geocoding home location".PHP_EOL.$sTxtForLogs); }
+
+// create an array to contain both coordinates objects
+$arrCoordinates[] = $cCoordSource;
+$arrCoordinates[] = $cCoordDest;
+
+// compute distance between source and destination
+$distances = $cOrs->distance($arrCoordinates, $source, $destination, $response);
+if ($distances != null && isset($distances)) {
+    echo "distance from $source to $destination: ".$distances[0]." ".$cOrs->getUnits();
+}
+
